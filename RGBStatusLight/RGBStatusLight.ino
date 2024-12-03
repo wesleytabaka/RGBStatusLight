ff074d3de87edc91cd69c58d109bc0a3db2741f5
Wesley Tabaka <wesley@wesleytabaka.com>
2024-12-02 21:08:11 -0500

    Move some comments around. Effects and transitions will be handled with Enums rather than strings.

--- RGBStatusLight/RGBStatusLight.ino
+++ RGBStatusLight/RGBStatusLight.ino
@@ -26,7 +26,7 @@ void setup() {
 
   // Start serial
   Serial.begin(9600);
-  
+
 }
 
 void setColor(int r, int g, int b){
@@ -42,12 +42,6 @@ void applyColor(){
   analogWrite(LED_B, blue);
 }
 
-// Color changing command:
-// TRANSITIONMODE, EFFECT, NEW_R, NEW_G, NEW_B | CYCLE
-// TRANSITIONMODE: NOW, FADE:DURATION
-// EFFECT: SOLID, BLINK:RATE, PULSE:RATE
-// CYCLE: Runs through RGB colors.
-
 void loop() {
 
   if(Serial.available()){    
@@ -55,15 +49,17 @@ void loop() {
     String input = Serial.readStringUntil('-');
     input.trim();
     if(input.length() > 0){
-      // example: NOW,SOLID,255,255,255
-  
       int field2 = input.indexOf(",", 0);
       int field3 = input.indexOf(",", field2 + 1);
       int field4 = input.indexOf(",", field3 + 1);
       int field5 = input.indexOf(",", field4 + 1);
       int field6 = input.indexOf(",", field5 + 1);
       int field7 = input.indexOf(",", field6 + 1);
-  
+
+      // Color changing command:
+      // TRANSITIONMODE: NOW: 0, FADE: 1
+      // EFFECT: SOLID: 0, BLINK: 1, PULSE: 2, CYCLE: 3
+      // example: 0,0,0,0,255,255,255
       String transition = input.substring(0, field2);
       String transitionDuration = input.substring(field2 + 1, field3);
       String effect = input.substring(field3 + 1, field4);
@@ -86,17 +82,12 @@ void loop() {
       serialOutput += g;
       serialOutput += ",";
       serialOutput += b;
-//  
-//      int sum = r.toInt() + g.toInt() + b.toInt();
   
       setColor(r.toInt(), g.toInt(), b.toInt());
       applyColor();
       Serial.println(serialOutput);
     }
-
   }
   
-  //applyColor();
   delay(3000);
-  
 }
