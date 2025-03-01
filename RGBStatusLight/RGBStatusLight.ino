acbeb24a91e1b8aeb12e70541d22d0269ceb8bb9
Wesley Tabaka <wesley@wesleytabaka.com>
2025-03-01 09:35:56 -0500

    Add a RunState to stopping/starting serial communication using sleep() and restart it using wake(). #7

--- RGBStatusLight/RGBStatusLight.ino
+++ RGBStatusLight/RGBStatusLight.ino
@@ -1,5 +1,6 @@
 #include "Effect.h"
 #include "Transition.h"
+#include "RunState.h"
 
 /* Pinout */
 const int LED_R = 6;
@@ -41,6 +42,8 @@ int transition_new_r = 0;
 int transition_new_g = 0;
 int transition_new_b = 0;
 
+RunState runState = RUN;
+
 /* Startup */
 void setup() {
   pinMode(LED_R, OUTPUT);
@@ -58,6 +61,18 @@ void setup() {
   // Start serial
   Serial.begin(9600);
 
+  runState = RUN;
+
+}
+
+void sleep(){
+  runState = SLEEP;
+  Serial.end();
+  delay(60000);
+}
+
+void wake(){
+  runState = RUN;
 }
 
 void setColor(int r, int g, int b){
@@ -203,71 +218,83 @@ void loop() {
     String input = Serial.readStringUntil('-');
     input.trim();
     if(input.length() > 0){
-      int field2 = input.indexOf(",", 0);
-      int field3 = input.indexOf(",", field2 + 1);
-      int field4 = input.indexOf(",", field3 + 1);
-      int field5 = input.indexOf(",", field4 + 1);
-      int field6 = input.indexOf(",", field5 + 1);
-      int field7 = input.indexOf(",", field6 + 1);
-      int field8 = input.indexOf(",", field7 + 1);
-      int field9 = input.indexOf(",", field8 + 1);
-      int field10 = input.indexOf(",", field9 + 1);
-
-      // Color changing command:
-      // TRANSITIONMODE: NOW: 0, FADE: 1
-      // EFFECT: SOLID: 0, BLINK: 1, PULSE: 2, CYCLE: 3
-      // example: 0,0,0,0,255,255,255
-      transition = (Transition) input.substring(0, field2).toInt();
-      transitionDuration = input.substring(field2 + 1, field3).toInt();
-      effect = (Effect) input.substring(field3 + 1, field4).toInt();
-      effectRate = input.substring(field4 + 1, field5).toInt();
-      String r = input.substring(field5 + 1, field6);
-      String g = input.substring(field6 + 1, field7);
-      String b = input.substring(field7 + 1, field8);
-      String r2 = input.substring(field8 + 1, field9);
-      String g2 = input.substring(field9 + 1, field10);
-      String b2 = input.substring(field10 + 1);
-
-      serialOutput = "OK:";
-      serialOutput += transition;
-      serialOutput += ",";
-      serialOutput += transitionDuration;
-      serialOutput += ",";
-      serialOutput += effect;
-      serialOutput += ",";
-      serialOutput += effectRate;
-      serialOutput += ",";
-      serialOutput += r;
-      serialOutput += ",";
-      serialOutput += g;
-      serialOutput += ",";
-      serialOutput += b;
-      serialOutput += ",";
-      serialOutput += r2;
-      serialOutput += ",";
-      serialOutput += g2;
-      serialOutput += ",";
-      serialOutput += b2;
-
-      resetEffect();
-      effect_basecolor_r = r.toInt();
-      effect_basecolor_g = g.toInt();
-      effect_basecolor_b = b.toInt();
-      effect_basecolor_r2 = r2.toInt();
-      effect_basecolor_g2 = g2.toInt();
-      effect_basecolor_b2 = b2.toInt();
-      effectPeriodLength = floor((float)effectRate / refreshRate);
-
-      resetTransition();
-      transition_old_r = abs(red-255);
-      transition_old_g = abs(green-255);
-      transition_old_b = abs(blue-255);
-      transition_new_r = r.toInt();
-      transition_new_g = g.toInt();
-      transition_new_b = b.toInt();
-      transitionPeriodLength = floor((float)transitionDuration / refreshRate);
-      
-      Serial.println(serialOutput);
+      if(input.indexOf("SLEEP", 0) >= 0 || input.indexOf("WAKE", 0) >= 0){
+        if(input.indexOf("SLEEP", 0) >= 0){
+          sleep();
+        }
+        else if(input.indexOf("WAKE", 0) >= 0){
+          wake();
+        }
+      }
+      else {
+        int field2 = input.indexOf(",", 0);
+        int field3 = input.indexOf(",", field2 + 1);
+        int field4 = input.indexOf(",", field3 + 1);
+        int field5 = input.indexOf(",", field4 + 1);
+        int field6 = input.indexOf(",", field5 + 1);
+        int field7 = input.indexOf(",", field6 + 1);
+        int field8 = input.indexOf(",", field7 + 1);
+        int field9 = input.indexOf(",", field8 + 1);
+        int field10 = input.indexOf(",", field9 + 1);
+  
+        // Color changing command:
+        // TRANSITIONMODE: NOW: 0, FADE: 1
+        // EFFECT: SOLID: 0, BLINK: 1, PULSE: 2, CYCLE: 3
+        // example: 0,0,0,0,255,255,255
+        transition = (Transition) input.substring(0, field2).toInt();
+        transitionDuration = input.substring(field2 + 1, field3).toInt();
+        effect = (Effect) input.substring(field3 + 1, field4).toInt();
+        effectRate = input.substring(field4 + 1, field5).toInt();
+        String r = input.substring(field5 + 1, field6);
+        String g = input.substring(field6 + 1, field7);
+        String b = input.substring(field7 + 1, field8);
+        String r2 = input.substring(field8 + 1, field9);
+        String g2 = input.substring(field9 + 1, field10);
+        String b2 = input.substring(field10 + 1);
+  
+        serialOutput = "OK:";
+        serialOutput += transition;
+        serialOutput += ",";
+        serialOutput += transitionDuration;
+        serialOutput += ",";
+        serialOutput += effect;
+        serialOutput += ",";
+        serialOutput += effectRate;
+        serialOutput += ",";
+        serialOutput += r;
+        serialOutput += ",";
+        serialOutput += g;
+        serialOutput += ",";
+        serialOutput += b;
+        serialOutput += ",";
+        serialOutput += r2;
+        serialOutput += ",";
+        serialOutput += g2;
+        serialOutput += ",";
+        serialOutput += b2;
+  
+        resetEffect();
+        effect_basecolor_r = r.toInt();
+        effect_basecolor_g = g.toInt();
+        effect_basecolor_b = b.toInt();
+        effect_basecolor_r2 = r2.toInt();
+        effect_basecolor_g2 = g2.toInt();
+        effect_basecolor_b2 = b2.toInt();
+        effectPeriodLength = floor((float)effectRate / refreshRate);
+  
+        resetTransition();
+        transition_old_r = abs(red-255);
+        transition_old_g = abs(green-255);
+        transition_old_b = abs(blue-255);
+        transition_new_r = r.toInt();
+        transition_new_g = g.toInt();
+        transition_new_b = b.toInt();
+        transitionPeriodLength = floor((float)transitionDuration / refreshRate);
+        
+        if(runState == RUN){
+          Serial.println(serialOutput);
+        }
+      }
     }
   }
 
---
+++ RGBStatusLight/RunState.h
@@ -0,0 +1,4 @@
+enum RunState {
+  RUN,
+  SLEEP
+};
